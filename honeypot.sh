e8108706c9ab46e4515ff45dc953b9e38478bf25
d213honeynet <107841716+d213honeynet@users.noreply.github.com>
2022-06-21 21:52:24 -0400

    Update honeypot.sh

--- honeypot.sh
+++ honeypot.sh
@@ -3,8 +3,8 @@
 echo "Instalasi Docker"
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings>
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.g>
 sudo apt update
 sudo apt install docker-ce
 sudo apt install docker-compose
@@ -14,6 +14,8 @@ sudo docker pull d213honeynet/cowrie:version1
 
 echo "instalasi Honeypot Dionaea"
 sudo docker pull d213honeynet/dionaea:version4
+sudo apt-get install unzip
+unzip dionaea.zip
 mkdir /home/${USER}/dionaea
 cp -r dionaea/ /home/${USER}
 
@@ -21,6 +23,6 @@ echo "instalasi Honeypot Glastopf"
 sudo docker pull d213honeynet/glastopf:version1
 
 echo "Instalasi Selesai"
-sudo docker-compose up
-cp cowrie/cowrie.cfg /var/lib/docker/volumes/honeypot_cowrie-etc/_data
+#sudo docker-compose up
+#cp cowrie/cowrie.cfg /var/lib/docker/volumes/honeypot_cowrie-etc/_data
 exit 0
